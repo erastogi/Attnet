d889e8682fe71195a774e59dd093f62fdfdc7e55
ilai-deutel <ilai-deutel@users.noreply.github.com>
2017-12-02 17:31:35 -0500

    AWS to git for attnet

--- Att_net/get_input_att.py
+++ Att_net/get_input_att.py
@@ -10,8 +10,15 @@ Created on Fri Dec  1 22:48:59 2017
 
 #saves dicitonaryies with keys :  features , object , atts -->  index values
 import numpy as np
+import torch
 from collections import defaultdict
 
+import data_loader as name
+import torch.nn as nn
+# name.load_data()
+
+
+print "blah"
 batch_size = 64
 num_batches = 517
 count = 0
@@ -21,23 +28,24 @@ test_id = np.load('test.npy')
 val_data = defaultdict(list)
 train_batch = defaultdict(list)
 test_data = defaultdict(list)
-
-for b in num_batches : 
-    ft , obj , att , vids = load_batch(b)
+print "start fetching data"
+for b in range(num_batches) : 
+    ft , obj , att , vids = name.load_batch(b)
+    print " batch loaded "
     vids = np.array(vids)
     #get indexes of common test data
     ind = np.nonzero(np.in1d(vids, val_id))[0]  
+    vids = np.delete(vids, ind)
+    print " train data ---" , len(val_data)
+    
     test_data['features'].append(ft[ind,:]) 
-    
-    
-    
     for i in ind :   
         #get index of ones
         if np.any(obj[i,:]==1) :
             obj_l = np.where(obj[i,:]==1)
         if np.any(att[i,:]==1) :   
             att_l = np.where(att[i,:]==1)           
-            
+           
       
         test_data['object'].append(obj_l)    
         test_data['atts'].append(att[i,:])
@@ -45,7 +53,10 @@ for b in num_batches :
     #get indexes of common valid data
     ind = np.nonzero(np.in1d(vids, val_id))[0]
     vids = np.delete(vids, ind)
+    print " valid data ---" , len(val_data)
+    
     val_data['features'].extend(ft[ind,:]) 
+    
     for i in ind :   
         #get index of ones
         if np.any(obj[i,:]==1) :
@@ -70,6 +81,7 @@ for b in num_batches :
         if len(train_batch) == batch_size :
              #save_this_batch
              np.save('train_batch' + str(count) + '.npy', train_batch)
+             print " train file saved "
              train_batch = defaultdict(list)
              count = count + 1
              
