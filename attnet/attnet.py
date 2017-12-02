9c37537854dc3174a6963dcb6f4545eb5019b353
ilai-deutel <ilai-deutel@users.noreply.github.com>
2017-12-02 00:29:15 -0500

    Proposal for losses

--- attnet/attnet.py
+++ attnet/attnet.py
@@ -11,22 +11,18 @@ from torch.autograd import Variable
 import torch.optim as optim
 import numpy as np
 
-#Declaring loss
-loss_obj = nn.NLLLoss()
-loss_attr = nn.BCELoss()
-
 
 class Attnet(nn.module) :
     #multi-task classification for objects and attributes
     #takes image features as input
-    
+
     def __init__(self, D_in , num_hid , num_classes ) :
         super(Attnet,self).__init__()
         self.num_layers = num_hid
         self.num_classes = num_classes
         self.D_in = D_in
-        
-    
+
+
         self.nn = nn.Sequential(
                   torch.nn.Linear(self.D_in, self.num_layer[0]),
                   torch.nn.ReLU(),
@@ -34,33 +30,33 @@ class Attnet(nn.module) :
                   torch.nn.ReLU())
                   #batch -norm
                   #torch.nn.BatchNorm1d(self.num_layer[1]))
-    
+
         ##intialise word embeddings for layer 2
         #pretrained_weight =
         #self.nn[1].weight.data.copy_(torch.from_numpy(pretrained_weight))
-        
+
         #for objects
         self.o = nn.Linear( self.num_layer[1] , self.num_classes[0])
-        self.ls = nn.LogSoftmax()
         #for attributes
         self.att = nn.Linear( self.num_layer[1] , self.num_classes[1])
-        self.sig = nn.Sigmoid()
-    
+
+        self.loss = nn.BCEWithLogitsLoss()
+
     def forward(self,input) :
-        
+
         r_out = self.nn(input)
-        p_ob = self.ls(self.o(r_out))
-        p_att = self.sig(self.att(r_out))
-        
+        p_ob = self.o(r_out)
+        p_att = self.att(r_out)
+
         return  [ p_ob , p_att ]
-    
-    
+
+
 def loss_func( p , y) :
 
       #object loss -- categorical cross entropy
-      l1 = loss_obj(p[0] , y[0])
+      l1 = loss(p[0] , y[0])
       #attribute loss -- binary cross entropy
-      l2 = loss_attr(p[1] , y[1])
-      
-      #return their sum      
+      l2 = loss(p[1] , y[1])
+
+      #return their sum
       return l1 + l2
