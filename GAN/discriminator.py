743598ab463ce188c9d683c47da60d3bbadf2feb
eti_r <eti_rastogi@yahoo.com>
2017-12-03 01:21:12 -0500

    test attent

--- GAN/discriminator.py
+++ GAN/discriminator.py
@@ -34,7 +34,7 @@ class Discriminator(nn.Module):
             return h.cuda()
         else:
             return h
-
+    """
     def forward(self, input, img_ft , hidden):
         # input dim                                                # batch_size x seq_len
         emb = self.embeddings(input)                               # batch_size x seq_len x embedding_dim
@@ -49,7 +49,25 @@ class Discriminator(nn.Module):
         out = torch.dot(img_ft , out)        
         out = F.sigmoid(out)
         return out
-
+    """
+    def forward(self, inp, hidden , input_lengths=None):
+        """
+        Embeds input and applies GRU one token at a time (seq_len = 1)
+        """
+        # input dim                                             # batch_size
+        emb = self.embeddings(inp)                              # batch_size x embedding_dim
+        emb = emb.view(1, -1, self.embedding_dim)               # 1 x batch_size x embedding_dim
+        ####
+        emb = nn.utils.rnn.pack_padded_sequence(emb, input_lengths, batch_first=True)
+        out, hidden = self.gru(emb, hidden)                     # 1 x batch_size x hidden_dim (out)
+        out , _ = nn.utils.rnn.pad_packed_sequence(out, batch_first=True)
+        ####        
+        out = self.gru2out(out.view(-1, self.hidden_dim))       # batch_size x vocab_size
+        out = torch.dot(img_ft , out)        
+        out = F.sigmoid(out)
+        return out
+    
+      
     def batchClassify(self, inp , img_ft ):
         """
         Classifies a batch of sequences.
@@ -61,11 +79,11 @@ class Discriminator(nn.Module):
             - out: batch_size ([0,1] score)
         """
 
-        h = self.init_hidden(inp.size()[0])
-        out = self.forward(inp,img_ft ,  h)
+        #h = self.init_hidden(inp.size()[0])
+        out = self.forward(inp,img_ft , None)
         return out.view(-1)
 
-    def batchBCELoss(self, inp, target):
+    def batchBCELoss(self, inp, img_ft  , target):
         """
         Returns Binary Cross Entropy Loss for discriminator.
 
@@ -76,7 +94,7 @@ class Discriminator(nn.Module):
 
         loss_fn = nn.BCELoss()
         
-        h = self.init_hidden(inp.size()[0])
-        out = self.forward(inp, h)
+        #h = self.init_hidden(inp.size()[0])
+        out = self.forward(inp,img_ft , None )
       
         return loss_fn(out, target)
